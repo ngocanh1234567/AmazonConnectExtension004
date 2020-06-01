0020f9014715b15065d4e22861463dd5069e389e
ngocanh1234567 <59072109+ngocanh1234567@users.noreply.github.com>
2020-06-01 14:12:27 +0700

    Delete cdk.ts

--- cdk/bin/cdk.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { AmazonConnectExtension004Stack } from '../lib/cdk-stack';
-
-const app = new cdk.App();
-new AmazonConnectExtension004Stack(app, 'AmazonConnectExtension004Stack');
